52d127093c5cb26e08379636dd63f15e35e54a50
meadch <meadch@gmail.com>
2020-11-11 10:55:03 -0800

    update db

--- src/clj/todos/db.clj
+++ src/clj/todos/db.clj
@@ -1,12 +1,24 @@
 (ns todos.db)
 
-(defonce items (atom (sorted-map)))
+(defonce items (atom {}))
+
+(defn now []
+  (java.time.LocalDateTime/now))
+
+(defn make-uuid []
+  (-> (java.util.UUID/randomUUID) str))
 
 (defn add-item [email item]
-  (let [email (keyword email)]
+  (let [owner (keyword email)
+        id (make-uuid)
+        created-at (str (now))]
     (swap! items (fn [items]
-                   (assoc-in items [email] (conj (email items) item))))))
+                   (assoc-in items [id] (merge item {:owner owner :created-at created-at :concluded-at nil}))))))
 
 (defn get-items-by-email [email]
-  (let [email (keyword email)]
-    (email @items)))
+  (filter #(= (keyword email) (:owner %)) (vals @items)))
+
+(comment
+  (add-item "meadch@gmail.com" {:text "Do a thing"})
+  (get-items-by-email "bob@gmail.com")
+  )
