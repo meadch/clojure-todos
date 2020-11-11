e3c65a12107c36bad58a86d3ffed827049f3d1bd
meadch <meadch@gmail.com>
2020-11-11 09:30:21 -0800

    Update clientside api

--- src/cljs/todos/api.cljs
+++ src/cljs/todos/api.cljs
@@ -1,8 +1,16 @@
 (ns todos.api
-  (:require [ajax.core :refer [GET POST]]))
+  (:require [ajax.core :refer [GET POST]]
+            [todos.state]))
+
+(defn handler [response]
+  (.log js/console response))
+
+(defn error-handler [{:keys [status status-text]}]
+  (.log js/console (str "something bad happened: " status " " status-text)))
 
 (defn create-todo! [msg]
   (POST "/api/todos" {:params {:msg msg} :handler (fn [r] (println r))}))
 
-(defn login! [email success]
-  (POST "/api/login" {:params {:email email} :handler success}))
+(defn fetch-todos [email]
+  (GET "/api/todos" {:params {:email email} :handler (fn [response]
+                                                       (reset! todos.state/items response))}))
