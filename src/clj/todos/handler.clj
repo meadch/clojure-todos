f80e2ebdadc22ba07b8533d39ac4d835eaae6c95
meadch <meadch@gmail.com>
2020-11-11 09:29:46 -0800

    Reconfigure middleware and hook up fetch-todos handler

--- src/clj/todos/handler.clj
+++ src/clj/todos/handler.clj
@@ -1,8 +1,11 @@
 (ns todos.handler
   (:require
    [reitit.ring :as reitit-ring]
-   [todos.middleware :refer [middleware]]
    [hiccup.page :refer [include-js include-css html5]]
+   [muuntaja.core :as m]
+   [reitit.ring.middleware.muuntaja :as muuntaja]
+   [reitit.ring.middleware.parameters :refer [parameters-middleware]]
+   [todos.db]
    [config.core :refer [env]]))
 
 (defn head []
@@ -26,17 +29,25 @@
    :body (loading-page)})
 
 (defn create-todo [_req] {:status 200 :body "Todo created!"})
-(defn login [_req] {:status 200 :body "Logged in!"})
+
+(defn fetch-todos [req] 
+  {
+   :status 200 
+   :body (todos.db/get-items-by-email (get (:query-params req) "email"))})
+
+(def router-config
+  {:data {:muuntaja m/instance
+          :middleware [parameters-middleware
+                       muuntaja/format-middleware]}})
 
 (def app
   (reitit-ring/ring-handler
    (reitit-ring/router
     [["/" {:get {:handler index-handler}}]
-     ["/todos" {:get {:handler index-handler}}]
+     ["/dashboard/:email" {:get {:handler index-handler}}]
      ["/api"
-      ["/todos" {:post {:handler create-todo}}]
-      ["/login" {:post {:handler login}}]]
-     ])
+      ["/todos" {:post {:handler create-todo} :get {:handler fetch-todos}}]]
+     ]
+    router-config)
    (reitit-ring/routes
-    (reitit-ring/create-resource-handler {:path "/" :root "/public"}))
-   {:middleware middleware}))
+    (reitit-ring/create-resource-handler {:path "/" :root "/public"}))))
