12d347a440328dcddcb3f155b0b09e60ae9122b2
meadch <meadch@gmail.com>
2020-11-12 09:32:11 -0800

    update charts

--- src/cljs/todos/line-chart.cljs
+++ src/cljs/todos/line-chart.cljs
@@ -3,29 +3,6 @@
    [cljsjs.react-vis :as rvis]
    [todos.state]))
 
-(def test-data '({:text "foo"
-                  :id "a7984057-f2a3-4b1e-b4cb-dffff132abef"
-                  :owner :charlie
-                  :created-at 1605145782373
-                  :concluded-at nil}
-                 {:text "bar"
-                  :id "bac646c7-5529-4822-8781-9a8d613dde3e"
-                  :owner :charlie
-                  :created-at 1605145785655
-                  :concluded-at nil}
-                 {:text "bizz"
-                  :id "1ad382eb-4ee2-4471-9356-5a365b487cd5"
-                  :owner :charlie
-                  :created-at 1605145804660
-                  :concluded-at 1605145830882}
-                 {:text "buzz"
-
-                  :id "11038c5b-e424-4cf2-91c6-830d8e6af1fc"
-                  :owner :charlie
-                  :created-at 1605145808137
-                  :concluded-at 1605145833158}))
-
-(.toLocaleDateString (js/Date. 1605145833158))
 (defn parse-items [items]
   (let [sorted-by-created-at (sort-by :created-at #(- %1 %2) items)
         events (mapcat (fn [todo]
@@ -38,23 +15,19 @@
                                           next-unfinished (if (:concluded event) (dec prev-unfinished) (inc prev-unfinished))]
                                       {:unfinished next-unfinished
                                        :data (conj (:data res) {:time (:time event) :unfinished next-unfinished})})) {:unfinished 0 :data []} sorted-by-time)]
-    (map-indexed (fn [i item] {:x (:time item) :y (:unfinished item)}) (:data unfinished-series))))
+    (map-indexed (fn [i item] {:x i :y (:unfinished item)}) (:data unfinished-series))))
 
-(def axis-style {:line {:stroke "#333"}
-                 :ticks {:stroke "#999"}
-                 :text {:stroke "none"
-                        :fill "#333"}})
+(def axis-style {:line {:stroke "#333"}})
 
 (defn component []
   (fn []
     [:div#line-chart.chart
-     [:h2 "Line Chart"]
+     [:h4 "Unfinished tasks over time"]
      [:> rvis/XYPlot
       {:width 250 :height 225}
-      [:> rvis/XAxis {:style axis-style :tick-format (fn [x] (.toLocaleDateString (js/Date. x)))}]
-      [:> rvis/YAxis {:style axis-style :y-domain [0,5]}]
+      [:> rvis/XAxis {:style axis-style :tick-format (fn [_] "")}]
+      [:> rvis/YAxis {:style axis-style :tick-format (fn [_] "")}]
       [:> rvis/LineSeries
        {:data (parse-items @todos.state/items)
         :color "#e47320"
-        :strokeWidth 5
         :style {:fill "none"}}]]]))
--- src/cljs/todos/pie-chart.cljs
+++ src/cljs/todos/pie-chart.cljs
@@ -1,6 +1,18 @@
-(ns todos.pie-chart)
+(ns todos.pie-chart
+  (:require
+   [cljsjs.react-vis :as rvis]
+   [todos.state :refer [items]]))
 
 (defn component []
-  (fn []
-    [:div.pie-chart.chart
-     [:h1 "Pie Chart"]]))
+   (fn []
+    [:div#line-chart.chart
+     [:h4 "Complete vs. incomplete tasks"]
+     (if-not (empty? @items) [:> rvis/RadialChart 
+      {:width 250 :height 225 :showLabels true
+       :data 
+       (let [
+             concluded-count (-> (filter :concluded-at @items) count)
+             unconcluded-count (- (count @items) concluded-count)]
+         [
+        {:angle concluded-count :label "Complete"} 
+        {:angle unconcluded-count :label "Incomplete"}])}])]))
