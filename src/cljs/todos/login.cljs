25133b03f46dcb2252783b55e9bbc59d7dd7f2af
meadch <meadch@gmail.com>
2020-11-13 05:19:42 -0800

    update login page styling

--- src/cljs/todos/login.cljs
+++ src/cljs/todos/login.cljs
@@ -6,18 +6,16 @@
 (defn login-input [value]
   [:input {:type "text"
            :value @value
-           :placeholder "email"
+           :placeholder "email address"
            :on-change #(reset! value (-> % .-target .-value))}])
 
 (defn component []
   (fn []
     (let [val (r/atom "")]
       (fn []
-        [:div.login
-         [:h1 "Login"]
+        [:section#login.card
          [login-input val]
-         [:input {:type "button" :value "Login"
-                  :on-click (fn []
+         [:button { :on-click (fn []
                               (let [email @val]
                                 (reset! val "")
-                                (accountant/navigate! (str "/dashboard/" email))))}]]))))
+                                (accountant/navigate! (str "/dashboard/" email))))} "Login" ]]))))
